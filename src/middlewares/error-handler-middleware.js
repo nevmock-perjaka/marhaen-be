1d47a529121cc365edb571ff8703f04f7198ada5
Faiz Elfahad Kurniawan <74404911+siunlucky@users.noreply.github.com>
2025-04-30 02:35:20 +0700

    first commit

--- src/middlewares/error-handler-middleware.js
+++ src/middlewares/error-handler-middleware.js
@@ -2,9 +2,8 @@ import logger from "../utils/logger.js";
 import BaseError from "../base_classes/base-error.js";
 import StatusCodes from "../errors/status-codes.js";
 import { INVALID_CREDENTIALS, SERVER_PROBLEM } from "../errors/error-codes.js";
-import SystemLog from "../models/systemLog.js";
-import User from "../models/user.js";
 import useragent from "useragent";
+import db from "../config/db.js";
 
 
 export const errorHandler = (err, req, res, _next) => {
@@ -51,11 +50,15 @@ export const errorHandler = (err, req, res, _next) => {
   console.error(err);
 
   // ambil user id
-  const user = User
-    .findOne({ email: req.body.email })
-    .select("_id")
+  const user = db.user.findUnique({
+    where: {
+      email: req.body.email,
+    },
+    select: {
+      id: true,
+    },
+  })
 
-  // ambil ip
   const ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
 
   // ambil device
