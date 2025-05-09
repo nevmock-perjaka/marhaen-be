23cdc2f64259e4823d054c809d8bcce9c5e6d137
Faiz Elfahad Kurniawan <74404911+siunlucky@users.noreply.github.com>
2025-05-09 19:10:37 +0700

    5.1.2 | Check smtp | Authentikasi & Otorisasi

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -5,8 +5,7 @@ const sendEmail = async (to, subject, text, link = null) => {
     try {
         const transporter = nodemailer.createTransport({
             service: "gmail",
-            port: 587,
-            secure: false,
+            secure: true,
             auth: {
                 user: process.env.EMAIL_USERNAME,
                 pass: process.env.EMAIL_PASSWORD,
@@ -29,7 +28,17 @@ const sendEmail = async (to, subject, text, link = null) => {
             ...(link && { html: link }),
         };
 
-        await transporter.sendMail(mailOptions);
+        await new Promise((resolve, reject) => {
+            transporter.sendMail(mailOptions, (err, info) => {
+                if (err) {
+                    console.error(err);
+                    reject(err);
+                } else {
+                    resolve(info);
+                }
+            });
+        });
+        // await transporter.sendMail(mailOptions);
         console.log("Email sent successfully!");
     } catch (error) {
         console.error("Error sending email:", error);
