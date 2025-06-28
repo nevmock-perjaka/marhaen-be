a900b35d8ac69f44f93a1027ddfb771ed9c2f385
mhndkptr <muhamadhendika77@gmail.com>
2025-06-28 10:11:46 +0700

    feat: search, filter, pagination in discount

--- src/domains/discount/discount-controller.js
+++ src/domains/discount/discount-controller.js
@@ -1,92 +1,90 @@
 import DiscountService from "./discount-service.js";
 import { successResponse, createdResponse } from "../../utils/response.js";
-import BaseError from "../../base_classes/base-error.js";
-import discountSchema from "./discount-schema.js";
 import Joi from "joi";
-import path from "path";
 
 class DiscountController {
-    async getAll(req, res) {
-        const userId =  req.user.id;
-        const discounts = await DiscountService.findAll(userId);
-        return successResponse(res, discounts);
+  async getAll(req, res) {
+    const userId = req.user.id;
+    const query = req.query;
+    const discounts = await DiscountService.findAll(userId, query);
+    return successResponse(res, discounts.data, discounts.count, discounts.meta);
+  }
+
+  async getById(req, res) {
+    const { id } = req.params;
+    const userId = req.user.id;
+    const discount = await DiscountService.findById(id, userId);
+    return successResponse(res, discount);
+  }
+
+  async create(req, res) {
+    const value = req.body;
+
+    value.owned_by = req.user.id;
+    value.created_by = req.profile.id;
+    value.updated_by = req.profile.id;
+    value.used = 0;
+
+    value.start_at = new Date(value.start_at);
+    value.expired_at = new Date(value.expired_at);
+
+    // console.log(new Date(), value.start_at);
+    if (value.start_at < new Date()) {
+      let validation = "";
+      let stack = [];
+      validation += "Start date must be greater than or equal to current date. ";
+
+      stack.push({
+        message: "Start date must be greater than or equal to current date.",
+        path: ["start_at"],
+      });
+
+      throw new Joi.ValidationError(validation, stack);
     }
 
-    async getById(req, res) {
-        const { id } = req.params;
-        const userId = req.user.id;
-        const discount = await DiscountService.findById(id, userId);
-        return successResponse(res, discount);
+    if (value.start_at >= value.expired_at) {
+      let validation = "";
+      let stack = [];
+
+      validation += "Start date must be less than expired date. ";
+
+      stack.push({
+        message: "Start date must be less than expired date.",
+        path: ["start_at"],
+      });
+
+      stack.push({
+        message: "Expired date must be greater than start date.",
+        path: ["expired_at"],
+      });
+
+      throw new Joi.ValidationError(validation, stack);
     }
 
-    async create(req, res) {
-        const value = req.body;
-        
-        value.owned_by = req.user.id;
-        value.created_by = req.profile.id;
-        value.updated_by = req.profile.id;
-        value.used = 0;
+    const created = await DiscountService.create(value);
+    return createdResponse(res, created);
+  }
 
-        value.start_at = new Date(value.start_at);
-        value.expired_at = new Date(value.expired_at);
+  async update(req, res) {
+    const { id } = req.params;
+    let value = req.body;
 
-        // console.log(new Date(), value.start_at);
-        if (value.start_at < new Date()) {
-            let validation = "";
-            let stack = [];
-            validation += "Start date must be greater than or equal to current date. ";
+    value.updated_by = req.profile.id;
+    value.owned_by = req.user.id;
 
-            stack.push({
-                message: "Start date must be greater than or equal to current date.",
-                path: ["start_at"]
-            });
+    value.start_at = new Date(value.start_at);
+    value.expired_at = new Date(value.expired_at);
 
-            throw new Joi.ValidationError(validation, stack);
-        }
+    const updated = await DiscountService.update(id, value);
+    return successResponse(res, updated);
+  }
 
-        if (value.start_at >= value.expired_at) {
-            let validation = "";
-            let stack = [];
-
-            validation += "Start date must be less than expired date. ";
-
-            stack.push({
-                message: "Start date must be less than expired date.",
-                path: ["start_at"]
-            });
-
-            stack.push({
-                message: "Expired date must be greater than start date.",
-                path: ["expired_at"]
-            })
-
-            throw new Joi.ValidationError(validation, stack);
-        }
-
-        const created = await DiscountService.create(value);
-        return createdResponse(res, created);
-    }
-
-    async update(req, res) {
-        const { id } = req.params;
-        let value = req.body;
-
-        value.updated_by = req.profile.id;
-        value.owned_by = req.user.id;
-
-        value.start_at = new Date(value.start_at);
-        value.expired_at = new Date(value.expired_at);
-
-        const updated = await DiscountService.update(id, value);
-        return successResponse(res, updated);
-    }
-
-    async delete(req, res) {
-        const { id } = req.params;
-        const userId = req.user.id;
-        const deleted = await DiscountService.delete(id, userId);
-        return successResponse(res, deleted);
-    }
+  async delete(req, res) {
+    const { id } = req.params;
+    const userId = req.user.id;
+    const deleted = await DiscountService.delete(id, userId);
+    return successResponse(res, deleted);
+  }
 }
 
 export default new DiscountController();
--- src/domains/discount/discount-service.js
+++ src/domains/discount/discount-service.js
@@ -1,119 +1,138 @@
 import db from "../../config/db.js";
 import BaseError from "../../base_classes/base-error.js";
 import Joi from "joi";
+import discountQueryConfig from "./discount-query-config.js";
+import { buildQueryOptions } from "../../utils/buildQueryOptions.js";
 
 class DiscountService {
-    async findAll(userId) {
-        return await db.discount.findMany({
-            where: { 
-                owned_by: userId
-             },
-            include: {
-                Product_discount: true,
-                Order: true,
-            },
-        });
-    }
+  async findAll(userId, query) {
+    const options = buildQueryOptions(discountQueryConfig, query, userId);
 
-    async findById(id, userId) {
-        const discount = await db.discount.findUnique({
-            where: { id },
-            include: {
-                Product_discount: true,
-                Order: true,
-            },
-        });
+    const [data, count] = await Promise.all([
+      db.discount.findMany(options),
+      db.discount.count({
+        where: options.where,
+      }),
+    ]);
 
-        if (!discount) {
-            throw BaseError.notFound("Discount not found.");
-        }
+    const currentPage = query?.pagination?.page ?? 1;
+    const itemsPerPage = query?.pagination?.limit ?? 10;
+    const totalPages = Math.ceil(count / itemsPerPage);
 
-        if (discount.owned_by !== userId) {
-            throw BaseError.forbidden("You are not allowed to access this discount.");
-        }
-
-        return discount;
-    }
-
-    async create(data) {
-        const existShareableCode = await db.discount.findFirst({
-            where: {
-                shareable_code: data.shareable_code,
-                owned_by: data.owned_by,
-            },
-        });
-
-        if (existShareableCode) throw BaseError.badRequest("Shareable code already exists.");
-
-        return await db.discount.create({ data });
-    }
-
-    async update(id, data) {
-        await this.checkPermission(id, data.owned_by);
-
-        const existsDiscount = await this.findById(id, data.owned_by);
-
-        if (existsDiscount.start_at.getTime() !== data.start_at.getTime()){
-            if (data.start_at < new Date()) {
-                let validation = "";
-                let stack = [];
-                validation += "Start date must be greater than or equal to current date. ";
-    
-                stack.push({
-                    message: "Start date must be greater than or equal to current date.",
-                    path: ["start_at"]
-                });
-    
-                throw new Joi.ValidationError(validation, stack);
+    return {
+      data,
+      meta:
+        query?.pagination?.page && query?.pagination?.limit
+          ? {
+              totalItems: count,
+              totalPages,
+              currentPage,
+              itemsPerPage,
             }
-        }
+          : null,
+      count: data.length,
+    };
+  }
 
-        if (data.start_at >= data.expired_at) {
-            let validation = "";
-            let stack = [];
+  async findById(id, userId) {
+    const discount = await db.discount.findUnique({
+      where: { id },
+      include: {
+        Product_discount: true,
+        Order: true,
+      },
+    });
 
-            validation += "Start date must be less than expired date. ";
-
-            stack.push({
-                message: "Start date must be less than expired date.",
-                path: ["start_at"]
-            });
-
-            stack.push({
-                message: "Expired date must be greater than start date.",
-                path: ["expired_at"]
-            })
-
-            throw new Joi.ValidationError(validation, stack);
-        }
-
-        return await db.discount.update({
-            where: { id },
-            data,
-        });
+    if (!discount) {
+      throw BaseError.notFound("Discount not found.");
     }
 
-    async delete(id, userId) {
-        await this.checkPermission(id, userId);
-
-        return await db.discount.delete({
-            where: { id }
-        });
+    if (discount.owned_by !== userId) {
+      throw BaseError.forbidden("You are not allowed to access this discount.");
     }
 
-    async checkPermission(id, userId) {
-        const discount = await db.discount.findUnique({
-            where: { id },
+    return discount;
+  }
+
+  async create(data) {
+    const existShareableCode = await db.discount.findFirst({
+      where: {
+        shareable_code: data.shareable_code,
+        owned_by: data.owned_by,
+      },
+    });
+
+    if (existShareableCode) throw BaseError.badRequest("Shareable code already exists.");
+
+    return await db.discount.create({ data });
+  }
+
+  async update(id, data) {
+    await this.checkPermission(id, data.owned_by);
+
+    const existsDiscount = await this.findById(id, data.owned_by);
+
+    if (existsDiscount.start_at.getTime() !== data.start_at.getTime()) {
+      if (data.start_at < new Date()) {
+        let validation = "";
+        let stack = [];
+        validation += "Start date must be greater than or equal to current date. ";
+
+        stack.push({
+          message: "Start date must be greater than or equal to current date.",
+          path: ["start_at"],
         });
 
-        if (!discount) {
-            throw BaseError.notFound("Discount not found.");
-        }
-
-        if (discount.owned_by !== userId) {
-            throw BaseError.forbidden("You are not allowed to access this discount.");
-        }
+        throw new Joi.ValidationError(validation, stack);
+      }
     }
+
+    if (data.start_at >= data.expired_at) {
+      let validation = "";
+      let stack = [];
+
+      validation += "Start date must be less than expired date. ";
+
+      stack.push({
+        message: "Start date must be less than expired date.",
+        path: ["start_at"],
+      });
+
+      stack.push({
+        message: "Expired date must be greater than start date.",
+        path: ["expired_at"],
+      });
+
+      throw new Joi.ValidationError(validation, stack);
+    }
+
+    return await db.discount.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async delete(id, userId) {
+    await this.checkPermission(id, userId);
+
+    return await db.discount.delete({
+      where: { id },
+    });
+  }
+
+  async checkPermission(id, userId) {
+    const discount = await db.discount.findUnique({
+      where: { id },
+    });
+
+    if (!discount) {
+      throw BaseError.notFound("Discount not found.");
+    }
+
+    if (discount.owned_by !== userId) {
+      throw BaseError.forbidden("You are not allowed to access this discount.");
+    }
+  }
 }
 
 export default new DiscountService();
