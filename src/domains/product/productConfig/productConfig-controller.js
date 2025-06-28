f3445afb694ea41f8b8e32926d7cfb4c45af6fe6
mhndkptr <muhamadhendika77@gmail.com>
2025-06-29 03:36:25 +0700

    feat: params in get product config

--- src/domains/product/productConfig/productConfig-controller.js
+++ src/domains/product/productConfig/productConfig-controller.js
@@ -1,90 +1,88 @@
 import db from "../../../config/db.js";
 import { successResponse, createdResponse } from "../../../utils/response.js";
-import BaseError from "../../../base_classes/base-error.js";
-import configSchema from "./productConfig-schema.js";
 import productConfigService from "./productConfig-service.js";
 import productService from "../product-service.js";
 import Joi from "joi";
 import inventoryService from "../../inventory/inventory-service.js";
 
 class ProductConfigController {
-    async getAll(req, res) {
-        const userId = req.user.id;
-        const configs = await productConfigService.findAll(userId);
+  async getAll(req, res) {
+    const userId = req.user.id;
+    const query = req.query;
+    const configs = await productConfigService.findAll(userId, query);
+    return successResponse(res, configs.data, configs.count, configs.meta);
+  }
 
-        return successResponse(res, configs);
+  async getById(req, res) {
+    const { id } = req.params;
+    const userId = req.user.id;
+    const config = await productConfigService.findById(id, userId);
+
+    return successResponse(res, config);
+  }
+
+  async create(req, res) {
+    let value = req.body;
+    const userId = req.user.id;
+    const profileId = req.profile.id;
+
+    value.owned_by = userId;
+    value.created_by = profileId;
+    value.updated_by = profileId;
+
+    try {
+      await productService.findById(value.product_id, req.user.id);
+    } catch (error) {
+      let validation = "";
+      let stack = [];
+
+      validation += "Product not found.";
+
+      stack.push({
+        message: "Product not found.",
+        path: ["product_id"],
+      });
+
+      throw new Joi.ValidationError(validation, stack);
     }
 
-    async getById(req, res) {
-        const { id } = req.params;
-        const userId = req.user.id;
-        const config = await productConfigService.findById(id, userId);
+    try {
+      await inventoryService.findById(value.inventory_id, req.user.id);
+    } catch (error) {
+      let validation = "";
+      let stack = [];
 
-        return successResponse(res, config);
+      validation += "Inventory not found.";
+
+      stack.push({
+        message: "Inventory not found.",
+        path: ["inventory_id"],
+      });
+
+      throw new Joi.ValidationError(validation, stack);
     }
 
-    async create(req, res) {
-        let value = req.body;
-        const userId = req.user.id;
-        const profileId = req.profile.id;
+    const created = await productConfigService.create(value);
+    return createdResponse(res, created);
+  }
 
-        value.owned_by = userId;
-        value.created_by = profileId;
-        value.updated_by = profileId;
+  async update(req, res) {
+    const { id } = req.params;
 
-        try {
-            await productService.findById(value.product_id, req.user.id);
-        } catch (error) {
-            let validation = "";
-            let stack = [];
+    let value = req.body;
+    value.updated_by = req.profile.id;
+    value.owned_by = req.user.id;
 
-            validation += "Product not found.";
+    const updated = await productConfigService.update(id, value);
+    return successResponse(res, updated);
+  }
 
-            stack.push({
-                message: "Product not found.",
-                path: ["product_id"]
-            });
+  async delete(req, res) {
+    const { id } = req.params;
+    await db.product_config.delete({ where: { id } });
 
-            throw new Joi.ValidationError(validation, stack);
-        }
-
-        try {
-            await inventoryService.findById(value.inventory_id, req.user.id);
-        } catch (error) {
-            let validation = "";
-            let stack = [];
-
-            validation += "Inventory not found.";
-
-            stack.push({
-                message: "Inventory not found.",
-                path: ["inventory_id"]
-            });
-
-            throw new Joi.ValidationError(validation, stack);
-        }
-
-        const created = await productConfigService.create(value);
-        return createdResponse(res, created);
-    }
-
-    async update(req, res) {
-        const { id } = req.params;
-        
-        let value = req.body;
-        value.updated_by = req.profile.id;
-        value.owned_by = req.user.id;
-        
-        const updated = await productConfigService.update(id, value);
-        return successResponse(res, updated);
-    }
-
-    async delete(req, res) {
-        const { id } = req.params;
-        await db.product_config.delete({ where: { id } });
-
-        return successResponse(res, { message: "Konfigurasi berhasil dihapus." });
-    }
+    return successResponse(res, { message: "Konfigurasi berhasil dihapus." });
+  }
 }
 
 export default new ProductConfigController();
---
+++ src/domains/product/productConfig/productConfig-query-config.js
@@ -0,0 +1,11 @@
+const productConfigQueryConfig = {
+  searchableFields: ["product.name"],
+  filterableFields: ["operation", "product_id", "inventory_id"],
+  hasSoftDelete: false,
+  relations: {
+    product: true,
+    inventory: true,
+  },
+};
+
+export default productConfigQueryConfig;
